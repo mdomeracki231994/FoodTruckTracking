178ce8b53bce5c5a2d17f281ad3d272188871477
CharlesDomeracki <itcfl@yahoo.com>
2024-01-09 07:53:51 -0500

    Bug fixes with bootstrap

--- Food_Truck_Tracking/settings.py
+++ Food_Truck_Tracking/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+
     # User Added Apps
     'user_management',
     'food_truck_admin',
@@ -121,9 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
